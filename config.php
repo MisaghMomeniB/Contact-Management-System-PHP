c995188eeef2a02a002dac31b52626dddb7882ef
MisaghMomeniB <jenabemisagh@gmail.com>
2025-08-02 11:27:27 +0330

    conf db

--- config.php
+++ config.php
@@ -1,1 +1,12 @@
 <?php
+$host = "";
+$dbname = "";
+$username = "";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
